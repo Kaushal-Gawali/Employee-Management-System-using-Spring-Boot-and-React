66d1bde2277b039d7213be807a14e2d4bbde4a4b
Kaushal <gawalikaushal7@gmail.com>
2025-10-19 11:02:41 +0530

    EMS

---
+++ full-stack/ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,16 @@
+// rafce = react arrow function component export
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbar-dark bg-dark'>
+                <a className="navbar-brand" href="https://www.javaguides.net">Employee Management System</a>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
